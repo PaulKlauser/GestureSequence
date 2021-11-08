3029d2dc8849ea92b2c5cda0b96c5b1785ec97ff
Paul Klauser <paul.klauser@gmail.com>
2021-11-08 00:03:51 -0500

    Remove Timber from sample

--- sample/src/main/java/com/paulklauser/sample/MainActivity.kt
+++ sample/src/main/java/com/paulklauser/sample/MainActivity.kt
@@ -4,12 +4,10 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.paulklauser.gesturesequence.GestureSequence
-import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Timber.plant(Timber.DebugTree())
         setContentView(R.layout.activity_main)
 
         GestureSequence.Builder(findViewById(R.id.hello_world))
